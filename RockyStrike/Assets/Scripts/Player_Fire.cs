299e138e3e1e47e8d2b29b228b6e7835f0e8b30a
ASAPRock <hyunrak.ko@partner.so>
2019-04-26 16:23:58 +0900

    2DStrike Project Upload

---
+++ RockyStrike/Assets/Scripts/Player_Fire.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fire : MonoBehaviour {
+
+    public GameObject PlayerBullet;
+    public Transform BulletLocation;
+    public float FireDelay;
+    private bool FireState;
+
+    public int BulletMaxPool;
+    private MemoryPool mPool;
+    private GameObject[] BulletArray;
+
+    private void OnApplicationQuit()
+    {
+        mPool.Dispose();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        FireState = true;
+
+        mPool = new MemoryPool();
+        mPool.Create(PlayerBullet, BulletMaxPool);
+        BulletArray = new GameObject[BulletMaxPool];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerFire();
+    }
+
+    private void PlayerFire()
+    {
+        if( FireState )
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                StartCoroutine(FireCycleControl());
+                
+                for(int i = 0; i < BulletMaxPool; i++)
+                {
+                    if( BulletArray[i] == null )
+                    {
+                        BulletArray[i] = mPool.NewItem();
+                        BulletArray[i].transform.position = BulletLocation.transform.position;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < BulletMaxPool; i++)
+        {
+            if(BulletArray[i])
+            {
+                if( BulletArray[i].GetComponent<Collider2D>().enabled == false )
+                {
+                    BulletArray[i].GetComponent<Collider2D>().enabled = true;
+                    mPool.RemoveItem(BulletArray[i]);
+                    BulletArray[i] = null;
+                }
+            }
+        }
+    }
+
+    IEnumerator FireCycleControl()
+    {
+        FireState = false;
+        yield return new WaitForSeconds(FireDelay);
+        FireState = true;
+    }
+}
---
+++ RockyStrike/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour {
+
+    public float Speed = 1f;	
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+		if( Input.GetKey(KeyCode.UpArrow) )
+        {
+            transform.Translate(Vector2.up * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        }
+    }
+}
